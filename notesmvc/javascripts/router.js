08790c7723ecedd2f9b05961e0a4314efc5d6d7e
Deepak Kumar <deepakkumarnd@gmail.com>
2014-02-09 23:42:01 +0530

    notes mvc initial

---
+++ notesmvc/javascripts/router.js
@@ -0,0 +1,13 @@
+App.Router.map(function() {
+    this.route('notes', {
+        path: '/notes'
+    });
+
+    this.route('bookmarks', {
+        path: '/bookmarks'
+    });
+
+    this.route('tasks', {
+        path: 'tasks'
+    });
+});
